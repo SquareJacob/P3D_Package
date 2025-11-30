0f3f72cde88b90f49a46c907f0089cc31975800a
SquareJacob <jacobascherzer@gmail.com>
2025-11-30 17:53:50 -0500

    webpage and summaries

--- src/P3D/graphing.py
+++ src/P3D/graphing.py
@@ -1,18 +1,34 @@
 import plotly.graph_objects as go
 import numpy as np
+from numpy.typing import NDArray
 from typing import List, Union
 
 
 class Figure(go.Figure):
-    """Assumes a 3D figure"""
+    """A 3D graph"""
     def __init__(self, data:List[go.Trace] = None, **kwargs):
-        """Assumes a 3D figure"""
+        """
+        Creates a 3D figure
+
+        Parameters
+        ----------
+        data
+            List of traces for this figure to have
+        """
         super().__init__(data = data, **kwargs)
         self.update_layout(
             scene_aspectmode='cube',
         )
 
-    def update_bounds(self, x:List[float] = None, y:List[float] = None, z:List[float] = None):
+    def update_bounds(self, x:List[float] = None, y:List[float] = None, z:List[float] = None) -> None:
+        """
+        Updates the bounds for this figure
+
+        Parameters
+        ----------
+        x,y,z
+            Bounds that can be updated, if provided, as a list of 2 numbers
+        """
         self.update_layout(
             scene = dict(
                 xaxis=dict(range=x),
@@ -21,7 +37,22 @@ class Figure(go.Figure):
             )
         )
 
-    def add_slider(self, slider_values:List[float], traces:List[Union[go.Trace, List[go.Trace]]], initial_step:int = 0, prefix:str = "t: "):
+    def add_slider(self, slider_values:List[float], traces:List[Union[go.Trace, List[go.Trace]]], initial_step:int = 0, prefix:str = "t: ") -> None:
+        """
+        Adds a new slider to this figure
+
+        Parameters
+        ----------
+        slider_values
+            List of N values the slider should have
+        traces
+            List of traces to add, and which slider value they should be visible on.
+            Each element can be a list itself, meaning multiple traces are visible on that value
+        initial_step
+            Which step (0 to N-1) to start the slider on
+        prefix
+            how each step on the slider is labelled
+        """
         steps = []
         N = len(self.data)
         A = sum([len(i) if isinstance(i, list) else 1 for i in traces])
@@ -47,12 +78,31 @@ class Figure(go.Figure):
         )
 
 class Surface(go.Surface):
-    def __init__(self, x, y, z, showscale = False, **kwargs):
-        """ """
+    """A 3D Surface"""
+    def __init__(self, x:NDArray[np.floating], y:NDArray[np.floating], z:NDArray[np.floating], showscale:bool = False, **kwargs):
+        """
+        Creates a 3D surface
+
+        Parameters
+        ----------
+        x,y,z
+            2D arrays all of the same shape. 
+            Each ordered triple is a point on the surface
+        showscale
+            Whether to show the colorbar on the side
+        """
         super().__init__(x = x, y = y, z = z, showscale = showscale, **kwargs)
 
 class Line(go.Scatter3d):
-    """Assumes a 3D line"""
-    def __init__(self, x, y, z, **kwargs):
-        """Assumes a 3D line"""
+    """A 3D line"""
+    def __init__(self, x:NDArray[np.floating], y:NDArray[np.floating], z:NDArray[np.floating], **kwargs):
+        """
+        Creates a 3D line
+
+        Parameters
+        ----------
+        x,y,z
+            1d arrays all of the same length.
+            Each ordered triple is a point on the line
+        """
         super().__init__(x = x, y = y, z = z, mode='lines', **kwargs)
--- src/P3D/webpage.py
+++ src/P3D/webpage.py
@@ -4,30 +4,75 @@ import numpy as np
 from typing import List, Union, Dict
 
 class Webpage(Dash):
-    """ """
+    """A Dash webpage"""
     def __init__(self, **kwargs):
-        """ """
+        """Creates A Dash webpage"""
         super().__init__(**kwargs)
     
 class Graph(dcc.Graph):
-    """ """
+    """Graph component for webpage"""
     def __init__(self, figure: go.Figure = None, id:str = None, height: float = None, **kwargs):
-        """ """
+        """
+        Creates a graph component for webpage
+
+        Parameters
+        ----------
+        figure
+            The figure to use in this graph
+        id
+            the unique id to identify this graph with
+        height
+            the proportion of the height of the window to take up; 0 to 1
+        """
         style = {'height': f'{int(100 * height)}vh'}
         if "style" in kwargs.keys():
             style = style | kwargs["style"]
         super().__init__(figure = figure, id = id, style = style, **kwargs)
 
 class DataTable(dash_table.DataTable):
-    """ """
-    def __init__(self, columns:List[str] = None, data: List[Union[List, Dict]] = None, id:str = None, height: float = None, properties: Dict[str, any] = None, **kwargs):
-        """ """
+    """Table component for webpage"""
+    def __init__(self, columns:List[str] = [], data: List[Union[List, Dict]] = [], id:str = None, height: float = None, column_ids:Dict[Union[str, int], str] = {}, properties: Dict[str, any] = {}, **kwargs):
+        """
+        Creates a table component for webpage
+
+        Parameters
+        ----------
+        columns
+            List of columns to use. 
+            Each element is the column's name and default id
+        data
+            Each element of list is one row of the table.
+            Element can be a list of values based upon column order,
+            or a dictionary of values with keys as column ids
+        id
+            the unique id to identify this graph with
+        height
+            the proportion of the height of the window to take up; 0 to 1
+        column_ids
+            ids to use instead of default ids.
+            Can specify column by its name or index
+        properties
+            Assigns properties to columns.
+            Each key is a property, and each value is what columns has that property.
+            Value can be True, meaning to apply to all columns.
+            Value can be a list of indices and/or ids to choose columns.
+            Common properties are editable, renamable, deletable, and clearable.
+        """
         column_data = []
+        ids = []
+        tID = id
         for i, name in enumerate(columns):
-            column = dict(id = name, name = name)
+            if i in column_ids.keys():
+                id = column_ids[i]
+            elif name in column_ids.keys():
+                id = column_ids[name]
+            else:
+                id = name
+            column = dict(id = id, name = name)
+            ids.append(id)
             for property, value in properties.items():
                 if isinstance(value, list):
-                    column[property] = (name in value) or (i in value)
+                    column[property] = (id in value) or (i in value)
                 else:
                     column[property] = value
             column_data.append(column)
@@ -36,11 +81,11 @@ class DataTable(dash_table.DataTable):
             frow = {}
             if isinstance(row, list):
                 for i in range(len(row)):
-                    frow[name[i]] = row[i]
+                    frow[ids[i]] = row[i]
             elif isinstance(row, dict):
                 frow = row
             row_data.append(frow)
         style_table = {'height': f'{int(100 * height)}vh'}
         if "style_table" in kwargs.keys():
             style_table = style_table | kwargs["style_table"]
-        super().__init__(columns=column_data, data=row_data, id = id, style_table= style_table, **kwargs)
+        super().__init__(columns=column_data, data=row_data, id = tID, style_table= style_table, **kwargs)
