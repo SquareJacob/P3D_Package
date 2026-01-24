42b84c7e4ffaba49e834003a369351ad767c1b24
SquareJacob <jacobascherzer@gmail.com>
2026-01-24 15:50:54 -0500

    bring back doc stuff

---
+++ docs/source/conf.py
@@ -0,0 +1,54 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'P3D'
+copyright = '2026, Jacob Scherzer'
+author = 'Jacob Scherzer'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.extlinks"
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+autodoc_member_order = 'bysource'
+#autodoc_typehints = 'none'
+autoclass_content = "both"   # include class docstring + __init__ docstring
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "plotly": ("https://plotly.com/python-api-reference/", None)
+}
+
+extlinks = {
+    "dash": ("https://dash.plotly.com/reference/#dash.%s", "dash.%s"),
+    "dcc": ("https://dash.plotly.com/dash-core-components/%s", "dash.dcc.%s"),
+    "html": ("https://dash.plotly.com/dash-html-components/%s", "html.%s"),
+    "table": ("https://dash.plotly.com/datatable", "dash.dash_table.DataTable")
+}
+
+rst_prolog = """
+.. |table| replace:: dash.dash_table.DataTable
+.. _table: https://dash.plotly.com/datatable
+"""
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/source/index.rst
@@ -0,0 +1,18 @@
+.. P3D documentation master file, created by
+   sphinx-quickstart on Sat Jan 24 12:57:52 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+P3D documentation
+=================
+
+Add your content using ``reStructuredText`` syntax. See the
+`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
+documentation for details.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api/modules
+
